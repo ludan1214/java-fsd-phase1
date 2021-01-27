204a0be2e2071a4adfb10463059393f029ca6c3e
root <root@LucPC.localdomain>
2021-01-27 15:33:09 -0600

    access Specifiers

---
+++ learnjava/src/learnjava/accessSpecifiers1.java
@@ -0,0 +1,10 @@
+package learnjava;
+
+public class accessSpecifiers1 {
+	public static void main(String[] args) {
+		//default
+		System.out.println("Default Access Specifier");
+		defAccessSpecifier obj = new defAccessSpecifier(); 		  
+        obj.display(); 
+	}
+}
---
+++ learnjava/src/learnjava/defAccessSpecifier.java
@@ -0,0 +1,8 @@
+package learnjava;
+
+class defAccessSpecifier {
+	void display() 
+    { 
+        System.out.println("You are using a default access specifier"); 
+    } 
+}
