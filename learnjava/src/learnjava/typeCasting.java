227ffa7b27c829415d6f985852083fab03fd488c
root <root@LucPC.localdomain>
2021-01-27 14:46:24 -0600

    typecasting example

---
+++ learnjava/src/learnjava/typeCasting.java
@@ -0,0 +1,27 @@
+package learnjava;
+
+public class typeCasting {
+	public static void main(String[] args) {
+		//Implicit Covnersion
+		System.out.println("Implicit Type Casting");
+		char a = 'A';
+		System.out.println("value of a: " + a);
+		int b = a;
+		System.out.println("value of b: " + b);
+		float c = a;
+		System.out.println("value of c: " + c);
+		long d = a;
+		System.out.println("value of d: " + d);
+		double e = a;
+		System.out.println("value of e: " + e);
+		
+		System.out.println("\n");
+		System.out.println("Explicit Type Casting");
+		// explicit conversion
+		
+		double x = 45.5;
+		int y = (int) x;
+		System.out.println("value of x: " + x);
+		System.out.println("value of x: " + y);
+	}
+}
