8dff23558e984067935a2ec7bfc8e2a60a2b581f
root <root@LucPC.localdomain>
2021-02-01 16:16:15 -0600

    added Regex

--- ValidateEmail/src/EmailValidator.java
+++ ValidateEmail/src/EmailValidator.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EmailValidator {
 	public boolean search(String query, String[] emailArr) {
@@ -17,10 +19,19 @@ public class EmailValidator {
 		if (args.length != 1) {
 			System.out.println("No arguments provided");
 		} else {
-			EmailValidator validator = new EmailValidator();
 			String mailQuery = args[0];	
-			System.out.println("Email Array: " + Arrays.toString(emails));
 			System.out.println("Email entered: " + mailQuery);
+			
+			
+			// Setup validation for the email query
+			EmailValidator validator = new EmailValidator();
+			String emailRegex = "^(.+)@(.+)$";
+			Pattern pattern = Pattern.compile(emailRegex);
+			Matcher matcher = pattern.matcher(mailQuery);
+			
+			System.out.println("Is a valid email: "+ matcher.matches());
+			System.out.println("Email Array to search: " + Arrays.toString(emails));
+			
 			System.out.println("Found: " + validator.search(mailQuery, emails));
 		}
 	}
