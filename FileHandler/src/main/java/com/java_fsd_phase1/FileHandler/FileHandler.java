53fda72314f0078d823ad3df55d54910eb7b79af
Luc <ludan1214@gmail.com>
2021-02-02 14:31:09 -0600

    File Handler Project init

---
+++ FileHandler/src/main/java/com/java_fsd_phase1/FileHandler/FileHandler.java
@@ -0,0 +1,7 @@
+package com.java_fsd_phase1.FileHandler;
+
+public class FileHandler {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ FileHandler/src/test/java/com/java_fsd_phase1/FileHandler/FileHandlerTest.java
@@ -0,0 +1,15 @@
+package com.java_fsd_phase1.FileHandler;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FileHandlerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+		
+	}
+
+}
